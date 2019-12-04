32fa3de5d24de458b9a5fc8d0265c02235c79e67
lunxinfeng <892452936@qq.com>
2019-12-04 16:58:39 +0800

    题目棋谱转方向

--- weiqi/weiqi/pipelines.py
+++ weiqi/weiqi/pipelines.py
@@ -53,19 +53,32 @@ class WeiqiPipeline(object):
                 """
         self.connect.execute(insert_sql,
                              {'book_name': book_name, 'book_sub_name': book_sub_name, 'course_index': course_index,
-                              'prepos_b': str(prepos_b), 'prepos_w': str(prepos_w), 'answers': str(answers), 'board_size': board_size,
-                              'black_first': black_first, 'qtypename': qtypename, 'levelname': levelname, 'title': title,
+                              'prepos_b': str(prepos_b), 'prepos_w': str(prepos_w), 'answers': str(answers),
+                              'board_size': board_size,
+                              'black_first': black_first, 'qtypename': qtypename, 'levelname': levelname,
+                              'title': title,
                               'status': status, 'sgf': sgf, 'signs': str(signs)})
         self.connect.commit()
         return item
 
     def getSgf(self, answers, black_first, prepos_b, prepos_w, signs, title):
+        array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"]
+        has_change = False
+        for p in prepos_b:
+            if array.index(p[0]) < array.__len__() / 2 < array.index(p[1]):
+                has_change = True
+                break
+        for p in prepos_w:
+            if array.index(p[0]) < array.__len__() / 2 < array.index(p[1]):
+                has_change = True
+                break
+
         sgf = "(;AB"
         for p in prepos_b:
-            sgf += ("[" + self.change(str(p)) + "]")
+            sgf += ("[" + (self.change(str(p)) if has_change else str(p)) + "]")
         sgf += "AW"
         for p in prepos_w:
-            sgf += ("[" + self.change(str(p)) + "]")
+            sgf += ("[" + (self.change(str(p)) if has_change else str(p)) + "]")
         if black_first == "1":
             sgf += "C[黑先"
         else:
@@ -117,9 +130,10 @@ class WeiqiPipeline(object):
         return sgf
 
     def change(self, s):
-        array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"]
-        first_index = array.index(s[0])
-        first_index = array.__len__() - 1 - first_index
-        second_index = array.index(s[1])
-        second_index = array.__len__() - 1 - second_index
-        return str(array[first_index]) + str(array[second_index])
+        # array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"]
+        # first_index = array.index(s[0])
+        # first_index = array.__len__() - 1 - first_index
+        # second_index = array.index(s[1])
+        # second_index = array.__len__() - 1 - second_index
+        # return str(array[second_index]) + str(array[first_index])
+        return str(s[1]) + str(s[0])
